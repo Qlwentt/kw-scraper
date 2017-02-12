a799695395a04abee35cd61cefc02d211fbce6b2
Qlwentt <qlwentt@gmail.com>
2017-02-11 20:35:45 -0800

    created kw-scraper

---
+++ api_wrapper.py
@@ -0,0 +1,55 @@
+from __future__ import unicode_literals
+import os
+from os.path import join, dirname
+
+from jobs.job_ad import JobAd
+
+from django.conf import settings
+
+import sys  
+import re
+
+import urllib2
+from textblob import TextBlob as tb
+from BeautifulSoup import BeautifulSoup
+import re
+import requests
+import unidecode
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+class QuallyApiWrapper(object):   
+	
+	@classmethod
+	def get_job_ads(cls, user_query, start):
+		base_url = "http://api.indeed.com/ads/apisearch?"
+		publisher = "publisher="+ os.environ["PUB_ID"]
+		# print publisher
+		rq_options = "sort=&radius=&st=&jt=&start={}&limit=1000&fromage=&filter=&latlong=1&co=us&chnl=".format(start)
+	
+		#need to ask someone to help me/figure out get real user ip address and agent
+		user_info="userip={}&useragent={}".format(user_query['ip'],user_query['user_agent'])
+		format_v= "v=2&format=json"
+
+		#search_term and city, state need to be dynamically inserted
+
+		"""user_query consists of
+			yrs_exp			int
+			search_term		string
+			city			string
+			state 			string"""
+		search_term=user_query['search_term']
+		city=user_query['city']
+		state=user_query['state']
+
+		# indeed_request_url= base_url+publisher+"&"+"q="+search_term+"&"+"l="+city+"%2C"+state+"&"+rq_options+"&"+user_info+"&"+format_v
+		indeed_request_url= "{base}{pub}&q={search_term}&l={city}%2C{state}&{request_ops}&{user_info}&{version}".format(
+			base=base_url,pub=publisher,search_term=search_term,city=city,state=state, request_ops=rq_options, user_info=user_info, version=format_v)
+		# print "indeed request url: {}".format(indeed_request_url) 
+		APIresponse=requests.get(indeed_request_url).json()['results']
+
+		# print "API response: {}".format(APIresponse)
+		return JobAd.make_job_ads_from_api(APIresponse)
+
+
+
---
+++ job_ad.py
@@ -0,0 +1,120 @@
+# _*_ coding:utf-8 _*_
+from __future__ import unicode_literals
+from collections import defaultdict
+
+import sys  
+import unidecode
+import re
+
+import urllib2
+from textblob import TextBlob as tb
+from BeautifulSoup import BeautifulSoup
+import re
+import requests
+import unidecode
+
+
+reload(sys)  
+sys.setdefaultencoding('utf8')
+
+
+
+class JobAd(object):
+	"""" A job ad from the Indeed API. It has the following properties:
+	Attributes:
+		# id: unique identitfier 		integer
+		key: unique id for API 		string
+		title: title of job ad; 	string
+		snippet: short description; string
+		url: url of full ad; 		string
+		company: company name;		string
+		content: full content of ad string 
+		location: City, State;		string
+		date: date posted;			datetime
+	"""
+
+
+	def __init__(self, properties):
+		#Returns job ad object with properties
+		self.key=properties['key']
+		self.title=properties['title']
+		self.snippet=properties['snippet']
+		self.content=''
+		self.url=properties['url']
+		self.company=properties['company']
+		self.location=properties['location']
+		self.date=properties['date']
+		self.score=0
+		self.matching_words=[]
+		self.exp_req= None
+		self.qually_rec = False
+		# self.keywords = []
+
+	@staticmethod
+	def fix_spacing(text):
+		  text=re.sub(r'([a-z:.)])([A-Z]|\d)', r'\1 \2', text)
+		  text=re.sub(r'([A-Z])(\d)', r'\1 \2', text)
+		  
+		  text=text.decode("utf-8").replace(u"\u2022", " ").encode("utf-8")
+		  return text
+
+	@classmethod
+	def make_job_ads_from_api(cls,api_json_resp):
+		job_ads=[]
+			
+		for result in api_json_resp:
+			# print "result: {}".format(result)
+			# print "result type: {}".format(type(result[0]))
+			job_ads.append(JobAd({
+				'key': result['jobkey'],
+				'title': result['jobtitle'],
+				'snippet': result['snippet'],
+				'url': result['url'],
+				'company': result['company'],
+				'location': result['formattedLocation'],
+				'date': result['date']
+				})) 
+		for job_ad in job_ads:
+			try:
+				cj=CachedJob.objects.get(key=job_ad.key)
+				job_ad.content = cj.content
+				#added this
+				job_ad.exp_req = cj.exp_req
+				job_ad.keywords = cj.keywords.all().values_list('name', flat=True)
+			except CachedJob.DoesNotExist:  
+				job_ad.content=job_ad.set_content()
+				#added this
+				print "I had to set experience required"
+				
+				job_ad.exp_req=job_ad.set_exp_req()
+				print "experience required is : ", job_ad.exp_req
+				job_ad.keywords=[]
+		return job_ads
+	
+	def set_content(self):	
+	    # self.content=job_ad_text
+	    html = urllib2.urlopen(self.url).read()
+	    soup = BeautifulSoup(html)
+	    job_ad_text = soup.find('span', attrs={'id': 'job_summary'}).text
+	    job_ad_text= JobAd.fix_spacing(job_ad_text)
+	    print job_ad_text
+	    return job_ad_text
+
+	@staticmethod
+	def to_s(tb):
+		return ('\t' + str(tb))
+	
+
+
+# # indeed_request_url="http://api.indeed.com/ads/apisearch?publisher=9253729351823762&q=software engineer&l=seattle%2C+wa&sort=&radius=&st=&jt=&start={}&limit=1000&fromage=&filter=&latlong=1&co=us&chnl=&userip=1.2.3.4&useragent=Mozilla/%2F4.0%28Firefox%29&v=2&format=json"
+# # APIresponse = requests.get(indeed_request_url).json()['results']
+# # test_job_ads=JobAd.make_job_ads_from_api(APIresponse)
+# # print test_job_ads
+# # print len(test_job_ads)
+# # print type(test_job_ads)
+# # print type(test_job_ads[0])
+
+# # print dir(test_job_ads[0])
+# # print test_job_ads[0].content
+
+
